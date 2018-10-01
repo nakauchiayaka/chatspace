903fe7e0c89a1b07e3163c86fa688e4cd9a1fd15
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-10-01 17:23:41 +0900

    非同期通信実装
    8. 7で作成したHTMLをメッセージ画面の一番下に追加する　まで完了

--- chat-space/app/assets/javascripts/chat.js
+++ chat-space/app/assets/javascripts/chat.js
@@ -1,4 +1,31 @@
 $(function(){
+  function buildHTML(chat){
+    if(chat.content.present?){
+      var html =
+    `<div class='chat-main__body--message__list'>
+      <p class='chat-main__body--message__list--name'>
+      ${chat.user_name}
+      </p>
+      <p class='chat-main__body--message__list--time'>
+      ${chat.created_at}
+      </p>
+      <div class='chat-main__body--message__list--body'>
+        <p class='chat-main__body--message__list--body__content'>
+        ${chat.content}
+        </p>
+      </div>
+    </div>`
+    if(chat.image.present?){
+      var html2 =
+      `<p class='chat-main__body--message__list--body__image'>
+      <%= image_tag ${chat.image.url} %>
+      </p>`
+    }
+    }
+
+    return html + html2;
+  }
+
   $('#new_chat').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +37,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__body').append(html)
+      $('.chat-main__footer--form__body--message').val('')
     })
   })
 })
