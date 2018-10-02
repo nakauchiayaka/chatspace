7d93accf4ac0e5885c0b171a222798b3cbc3f266
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-10-02 14:14:08 +0900

    メッセージ送信後の処理まで完了
    HTMLを追加した分、メッセージ画面を下にスクロールするまで完了

--- chat-space/app/assets/javascripts/chat.js
+++ chat-space/app/assets/javascripts/chat.js
@@ -1,8 +1,26 @@
 $(function(){
   function buildHTML(chat){
-    if(chat.content.present?){
-      var html =
-    `<div class='chat-main__body--message__list'>
+    if(chat.content){
+      if(chat.image.url){
+        var html = `<div class='chat-main__body--message__list'>
+      <p class='chat-main__body--message__list--name'>
+      ${chat.user_name}
+      </p>
+      <p class='chat-main__body--message__list--time'>
+      ${chat.created_at}
+      </p>
+      <div class='chat-main__body--message__list--body'>
+        <p class='chat-main__body--message__list--body__content'>
+        ${chat.content}
+        </p>
+        <img class="chat-main__body--message__list--body__image" src= ${chat.image.url} >
+      </div>
+    </div>`
+    return html;
+
+      }
+      else{
+        var html = `<div class='chat-main__body--message__list'>
       <p class='chat-main__body--message__list--name'>
       ${chat.user_name}
       </p>
@@ -15,15 +33,26 @@ $(function(){
         </p>
       </div>
     </div>`
-    if(chat.image.present?){
-      var html2 =
-      `<p class='chat-main__body--message__list--body__image'>
-      <%= image_tag ${chat.image.url} %>
-      </p>`
-    }
-    }
+    return html;
 
-    return html + html2;
+      }
+    }
+    else if(chat.image.url){
+      var html =
+      `<div class='chat-main__body--message__list'>
+      <p class='chat-main__body--message__list--name'>
+      ${chat.user_name}
+      </p>
+      <p class='chat-main__body--message__list--time'>
+      ${chat.created_at}
+      </p>
+      <div class='chat-main__body--message__list--body'>
+      <img class="chat-main__body--message__list--body__image" src= ${chat.image.url} >
+      </div>
+    </div>`
+    return html;
+
+    }
   }
 
   $('#new_chat').on('submit', function(e){
@@ -40,8 +69,14 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.chat-main__body').append(html)
-      $('.chat-main__footer--form__body--message').val('')
+      $('.chat-main__body').append(html);
+      console.log("gggggg");
+      $('.chat-main__footer--form__body--message').val('');
+      $('.chat-main__body').animate({scrollTop:9999});
+      $('.chat-main__footer--form__body--submit').attr('disabled', false);
+    })
+    .fail(function(){
+      alert('メッセージを入力してください。');
     })
   })
 })
