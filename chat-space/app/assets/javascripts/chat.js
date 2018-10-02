65baf609d797851c6004f9b1559cfde9518308fe
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-10-02 15:37:32 +0900

    指摘箇所修正
    冗長なコード修正

--- chat-space/app/assets/javascripts/chat.js
+++ chat-space/app/assets/javascripts/chat.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(chat){
-     var html = `<div class='chat-main__body--message__list'>
+    var html_top = `<div class='chat-main__body--message__list'>
       <p class='chat-main__body--message__list--name'>
       ${chat.user_name}
       </p>
@@ -8,37 +8,23 @@ $(function(){
       ${chat.created_at}
       </p>
       <div class='chat-main__body--message__list--body'>`
+    var content_html = `<p class='chat-main__body--message__list--body__content'>
+      ${chat.content}
+      </p>`
+    var image_html = `<img class="chat-main__body--message__list--body__image" src= ${chat.image.url} >`
+    var html_bottom = `</div>
+      </div>`
 
     if(chat.content){
       if(chat.image.url){
-        var content_image_html = `
-        <p class='chat-main__body--message__list--body__content'>
-        ${chat.content}
-        </p>
-        <img class="chat-main__body--message__list--body__image" src= ${chat.image.url} >
-      </div>
-    </div>`
-    return html + content_image_html;
+          return html_top + content_html + image_html + html_bottom;
       }
       else{
-        var content_html = `
-        <p class='chat-main__body--message__list--body__content'>
-        ${chat.content}
-        </p>
-      </div>
-    </div>`
-    return html + content_html;
-
+          return html_top + content_html + html_bottom;
       }
     }
     else if(chat.image.url){
-      var image_html =
-      `
-      <img class="chat-main__body--message__list--body__image" src= ${chat.image.url} >
-      </div>
-    </div>`
-    return html + image_html;
-
+        return html_top + image_html + html_bottom;
     }
   }
 
