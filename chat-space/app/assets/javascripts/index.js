5e9210d557d9219c7e9d0b87f2513e35a4adf27b
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-10-03 10:35:36 +0900

    エラー時の処理を行うまで完了

--- chat-space/app/assets/javascripts/index.js
+++ chat-space/app/assets/javascripts/index.js
@@ -1,5 +1,45 @@
 $(function() {
+  var search_list = $('.user-search-result');
+  function appendUser(user){
+    var html =
+    `
+    <div class='chat-group-user clearfix'>
+    <p class='chat-group-user__name'>${user.name}</p>
+    <a class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id='ユーザーのid' data-user-name='ユーザー名'>追加</a>
+    </div>
+    `
+    search_list.append(html);
+  }
+  function appendNoUser(user){
+    var html = `
+    <div class='chat-group-user clearfix'>
+    <p class='chat-group-user__name'>${user}</p>
+    </div>`
+    search_list.append(html);
+  }
   $(".chat-group-form__input").on("keyup", function() {
-    var input = $(".chat-group-form__input").val();
+    var input = $(".chat-group-form__input--user").val();
+    console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/users/',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users) {
+     $(".user-search-result").empty();
+     if (users.length !== 0) {
+       users.forEach(function(user){
+         appendUser(user);
+       });
+     }
+     else {
+       appendNoUser("一致するユーザーはいません");
+     }
+   })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
   });
 });
