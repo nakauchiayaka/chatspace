35e9d3dcb1cab48bd286d4bb4accd7fcd4334868
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-10-03 13:29:55 +0900

    検索結果のHTMLからは消すまで完了

--- chat-space/app/assets/javascripts/index.js
+++ chat-space/app/assets/javascripts/index.js
@@ -3,9 +3,9 @@ $(function() {
   function appendUser(user){
     var html =
     `
-    <div class='chat-group-user clearfix'>
-    <p class='chat-group-user__name'>${user.name}</p>
-    <a class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id='ユーザーのid' data-user-name='ユーザー名'>追加</a>
+    <div class='chat-group-user chat-group-user__add clearfix'>
+    <p class='chat-group-user__name '>${user.name}</p>
+    <a class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id='${user.id}' data-user-name='${user.name}'>追加</a>
     </div>
     `
     search_list.append(html);
@@ -17,6 +17,11 @@ $(function() {
     </div>`
     search_list.append(html);
   }
+  function removeUser(user){
+    var html = ``
+    search_list.append(html);
+  }
+
   $(".chat-group-form__input").on("keyup", function() {
     var input = $(".chat-group-form__input--user").val();
     console.log(input);
@@ -32,6 +37,9 @@ $(function() {
      if (users.length !== 0) {
        users.forEach(function(user){
          appendUser(user);
+         $(".user-search-result").on("click",".user-search-add",function(){
+            $('.chat-group-user__add').empty();
+          });
        });
      }
      else {
@@ -43,7 +51,5 @@ $(function() {
     })
   });
 
-  $(".user-search-result").on("click",".user-search-add",function(){
-    console.log("abc");
-  });
+
 });
