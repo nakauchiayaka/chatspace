b7307621ac1921e5c634f75845372341b7969696
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-10-06 15:04:27 +0900

    インクリメンタルサーチ実装

--- chat-space/app/assets/javascripts/index.js
+++ chat-space/app/assets/javascripts/index.js
@@ -17,10 +17,10 @@ $(function() {
     </div>`
     search_list.append(html);
   }
-  function addChatMember(user){
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
-  <p class='chat-group-user__name'>${user.name}</p>
+  function addChatMember(id,name){
+    var html = `<div class='chat-group-user chat-group-user__remove clearfix js-chat-member' id='chat-group-user-8'>
+  <input name='group[user_ids][]' type='hidden' value=${id}>
+  <p class='chat-group-user__name'>${name}</p>
   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
 </div>`
     $('#chat-group-users').append(html);
@@ -28,7 +28,6 @@ $(function() {
 
   $(".chat-group-form__input").on("keyup", function() {
     var input = $(".chat-group-form__input--user").val();
-    console.log(input);
 
     $.ajax({
       type: 'GET',
@@ -41,15 +40,22 @@ $(function() {
      if (users.length !== 0) {
        users.forEach(function(user){
          appendUser(user);
-         $(".user-search-result").on("click",".user-search-add",function(){
-            $('.chat-group-user__add').empty();
-            addChatMember(user);
-          });
+
        });
      }
      else {
        appendNoUser("一致するユーザーはいません");
      }
+
+     $(".chat-group-user__add").on("click",".user-search-add",function(){
+            var id =  $(this).attr("data-user-id");
+            var name = $(this).attr("data-user-name");
+            $(this).parent().empty();
+            addChatMember(id,name);
+          });
+         $(".chat-group-form__field--right").on("click",".js-remove-btn",function(){
+            $(this).parent().empty();
+         });
    })
     .fail(function() {
       alert('ユーザー検索に失敗しました');
