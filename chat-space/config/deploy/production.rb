ee2de3aedd3ab0ae793f77428f6912528ffb3f7f
nakauchiayaka <ayakaaaaa61@gmail.com>
2018-10-11 15:49:23 +0900

    capistrano編集

--- chat-space/config/deploy/production.rb
+++ chat-space/config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.194.18.39', user: 'ec2-user', roles: %w{app db web}
+server '54.64.125.6', user: 'ec2-user', roles: %w{app db web}
